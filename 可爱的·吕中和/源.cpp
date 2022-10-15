66ce19345451850764549b7f05be691e392071a7
Your Name <you@example.com>
2022-10-15 18:14:16 +0800

    1

---
+++ 可爱的·吕中和/源.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	printf("亲爱的吕：\n");
+	printf("宝贝，我好喜欢秋天。\n");
+	printf("不能和你一起欣赏美丽的秋景了。\n");
+	printf("不过，这说明我们即将迎来重逢的日子。\n");
+	printf("我好想你！\n");
+	printf("              --轩.\n");
+	return 0;
+
+ }
